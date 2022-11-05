966cfb3e972e67f98ff65f0ed62b56a743aa8b01
FIJI <117532602+FIJILeaks@users.noreply.github.com>
2022-11-05 17:45:21 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('Coder is Online!');
+});
+
+
+
+client.login('ODQxNzgzMzk0Njc3NTU1MjAw.GoUOMt.vHmOpKSihh3LeGzeEdeKZ__M1Ty1sM86Xlh2fc');
+
